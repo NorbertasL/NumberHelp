47ccad308d58601f327ef0f4be2dc33a8a0feb9e
Red_Spark <RedSparkdev@gmail.com>
2017-08-02 13:25:45 +0100

    Added fragments to activity for testing

--- app/src/main/java/com/example/red_spark/numberhelp/MainActivity.java
+++ app/src/main/java/com/example/red_spark/numberhelp/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.red_spark.numberhelp;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentManager mFragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //checking for save instance state
+        if(savedInstanceState == null) {
+            mFragmentManager = getSupportFragmentManager();
+            mFragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, new ValueFagment(), "TestFrag1")
+                    .add(R.id.fragment_container, new ValueFagment(), "TestFrag2")
+                    .commit();
+        }
+
+
     }
 }
