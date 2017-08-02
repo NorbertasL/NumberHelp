66b7e8aa4189cc609bde4917cb8db6d1bf02ab17
Red_Spark <RedSparkdev@gmail.com>
2017-08-02 13:28:19 +0100

    Added Butterknife dependencies

--- app/src/main/java/com/example/red_spark/numberhelp/ValueFagment.java
+++ app/src/main/java/com/example/red_spark/numberhelp/ValueFagment.java
@@ -6,12 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ValueFagment extends Fragment {
+    private EditText mTextField;
+    private Button mConvertButton;
 
 
     public ValueFagment() {
